96577928c327ab4f379a7df8612dc425a51f5212
niranjanbasnet <niranjanbasnet@hotmail.com>
2015-07-08 20:34:37 +0200

    This is complete change of requirement.
    Change of codes.

--- TeamProject/TeamProject/MainPage.xaml.cs
+++ TeamProject/TeamProject/MainPage.xaml.cs
@@ -13,10 +13,9 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
 
-namespace AttackingSmartphone
+namespace TeamProject
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
--- TeamProject/TeamProject/Properties/AssemblyInfo.cs
+++ TeamProject/TeamProject/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AttackingSmartphone")]
+[assembly: AssemblyTitle("TeamProject")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AttackingSmartphone")]
+[assembly: AssemblyProduct("TeamProject")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
